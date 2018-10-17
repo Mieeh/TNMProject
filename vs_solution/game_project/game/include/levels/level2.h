1f12e79e8f012b42a4443b429a1f0afafc9e7efd
RobotGandhi <99viktor.vc@gmail.com>
2018-10-17 11:02:29 +0200

    Despacito

---
+++ vs_solution/game_project/game/include/levels/level2.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include"../../../engine/include/level.h"
+#include"../../../engine/include/game_systems.h"
+
+#include"level_utility.h"
+#include "../player.h"
+
+struct Level2 : ILevel {
+
+	LevelContent content;
+
+	void init() override {
+		content.tile_map = LEVEL_2;
+		levelUtility_ConvertToLevelContent(content);
+
+		Player::get()->play_intro_at(core::Vector2i(3, 3));
+
+		GraphicsSingleton::Instance()->point_to_follow = &Player::get()->world_position;
+	}
+
+	void on_event(Event& event) {
+		if (event.type == EventType::KeyPressed) {
+			if (event.key == Key::X)
+				Player::get()->play_intro_at(core::Vector2i(3, 3));
+		}
+	}
+
+	void update(float dt) override {
+
+	}
+
+	void render() override {
+		GraphicsSingleton::Instance()->draw(content.walls_floors); // Render all the entities here
+		GraphicsSingleton::Instance()->draw(content.enemies);
+	}
+
+	void player_moved() override {
+
+	}
+
+	LevelContent& get_level_content()
+	{
+		return content;
+	}
+
+};
